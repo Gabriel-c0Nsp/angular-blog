2d3b0b5e0f083dfbf5c7536062f81ba96ced1368
Gabriel-c0Nsp <gabrielsilvaaires62@gmail.com>
2023-09-12 02:59:15 -0300

    Título concluído. Parte do layout do card principal feito.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { TitleComponent } from './components/title/title.component';
+import { MainCardComponent } from './components/main-card/main-card.component';
+import { NormalCardComponent } from './components/normal-card/normal-card.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    TitleComponent,
+    MainCardComponent,
+    NormalCardComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
